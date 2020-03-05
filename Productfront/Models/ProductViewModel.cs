1615710ce0963b5396ecf499fd3fe24472ed5864
00004047 <33658166+00004047@users.noreply.github.com>
2020-03-05 21:29:50 +0500

    first commit

---
+++ Productfront/Models/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Productfront.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+        public Category category { get; set; }
+
+    }
+}
